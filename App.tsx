b5eb3667655c5f03df4a3bda1db9a6479194f26b
frenetico9 <hiago182016@gmail.com>
2025-07-09 10:35:49 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -23,6 +23,7 @@ import { AddBillModal } from './components/modals/AddBillModal';
 import { AssetsView } from './components/AssetsView';
 import { AddAssetModal } from './components/modals/AddAssetModal';
 import { ReportsView } from './components/ReportsView';
+import { InstallHelpModal } from './components/modals/InstallHelpModal';
 
 
 const App: React.FC = () => {
@@ -65,6 +66,9 @@ const App: React.FC = () => {
     // Assets
     saveAsset,
     deleteAsset,
+    // PWA Install
+    showInstallHelpModal,
+    closeInstallHelpModal,
   } = useAuth();
 
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -264,6 +268,8 @@ const App: React.FC = () => {
       </button>
       
       {/* --- Modals --- */}
+      {showInstallHelpModal && <InstallHelpModal onClose={closeInstallHelpModal} />}
+
       {(addTransactionModalOpen || editingTransaction) && (
         <AddTransactionModal 
             onClose={() => { setAddTransactionModalOpen(false); setEditingTransaction(null); }} 
